facecca44e7ae04ab67332cd047b41247a0bd368
Isarok <isaac.lucerito@gmail.com>
2024-09-10 14:48:20 +0200

    feature/desing_addSomestyles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
               className="mb-4 text-[26px] lg:text-[50px] 2xl:text-[60px]  uppercase leading-[1] font-semibold"
             >
               <span className="mr-2">Soy</span>
-              <span className="text-cyan-400">Isaac Garcia Romero.</span>
+              <span className="bg-gradient-to-r from-[rgba(2,165,184,1)] from-[10%] via-[rgba(0,228,255,1)] via-[30%] to-[rgba(152,251,255,1)] to-[90%] text-transparent bg-clip-text">Isaac Garcia Romero.</span>
             </motion.div>
 
             <motion.p
@@ -35,11 +35,11 @@ const Main = () => {
               sm:max-w-sm 
               2xl:mx-0 2xl:max-w-3xl 2xl:text-xl 2xl:max-w-[1200px] "
             >
-              Graduado de un bootcamp de 8 meses Fullstack + Cyberseguridad con
-              F5. Poseo habilidades sólidas en desarrollo web y una gran ética
-              de ambientelaboral. Mi enfoque versátil me permite crear
+              Graduado de un bootcamp de 8 meses <span className="text-cyan-200">Fullstack + Cyberseguridad con
+              Factoria F5.</span> Poseo habilidades sólidas en <span className="text-cyan-200">desarrollo web</span>  y una <span className="text-cyan-200">gran ética
+              de ambientelaboral</span> . Mi enfoque versátil me permite crear
               aplicaciones web funcionales y atractivas. Estoy comprometido con
-              el aprendizaje continuo y estoy dispuesto a poner en practica
+              el<span className="text-cyan-200">aprendizaje continuo </span> y estoy dispuesto a poner en practica
               todas mis habilidades al servicio de cualquier desafio.
             </motion.p>
 
@@ -72,13 +72,22 @@ const Main = () => {
                   </div>
               </span>
               <ul className="flex justify-around m-4 md:m-0 gap-x-10 gap-y-2">
-                <li>
+                <li >
                 <a
                   href="https://docs.google.com/document/d/1pF7dyoWNfUFHGvCdg0XtNNPOKMay5a6l/edit?usp=sharing&ouid=105822291999599368082&rtpof=true&sd=true"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <PiReadCvLogo  className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
+                    <svg width="0" height="0">
+                      <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%" x3="0%" y3="0%">
+                      <stop offset="0%" style={{ stopColor: "rgba(2,165,184,1)", stopOpacity: 1 }} />
+                        <stop offset="50%" style={{ stopColor: "rgba(0,228,255,1)", stopOpacity: 1 }} />
+                        <stop offset="100%" style={{ stopColor: "rgba(152,251,255,1)", stopOpacity: 1 }} />
+                      </linearGradient>
+                    </svg>
+                  <PiReadCvLogo style={{ fill: "url(#grad1)" }}
+                                type="button"
+                                className="text-4xl 2xl:text-5xl  hover:scale-x-150 hover:scale-y-150 	" />
                 </a>
                 </li>
                 <li>
@@ -87,7 +96,9 @@ const Main = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <FaGithub className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
+                  <FaGithub style={{ fill: "url(#grad1)" }}
+                            type="button"
+                            className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
                 </a>
                 </li>
                 <li>
@@ -96,7 +107,9 @@ const Main = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <FaDiscord className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
+                  <FaDiscord style={{ fill: "url(#grad1)" }}
+                            type="button" 
+                            className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
                 </a>
                 </li>
                 <li>
@@ -105,7 +118,9 @@ const Main = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <FaLinkedin className="text-4xl 2xl:text-5xl text-cyan-400 hover:scale-x-150 hover:scale-y-150	" />
+                  <FaLinkedin style={{ fill: "url(#grad1)" }}
+                              type="button" 
+                              className="text-4xl 2xl:text-5xl icon-gradient hover:scale-x-150 hover:scale-y-150	" />
                 </a>
                 </li>
               </ul>
